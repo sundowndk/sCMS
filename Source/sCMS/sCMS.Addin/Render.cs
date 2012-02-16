5bd96bf6af2f16bd2abc17805497f28b99e0af2c
Rasmus Pedersen <rasmus@akvaservice.dk>
2012-02-16 23:42:50 +0100

    -

--- Source/sCMS/sCMS.Addin/Render.cs
+++ Source/sCMS/sCMS.Addin/Render.cs
@@ -2,20 +2,20 @@
 // Render.cs
 //
 // Author:
-//       Rasmus Pedersen <rasmus@akvaservice.dk>
-// 
+// Rasmus Pedersen <rasmus@akvaservice.dk>
+//
 // Copyright (c) 2009 Rasmus Pedersen
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,209 +32,209 @@ using SorentoLib;
 
 namespace sCMS.Addin
 {
-	public class Render : SorentoLib.Addins.IRenderBaseClass, SorentoLib.Addins.IRender
-	{
-		#region Constructor
-		public Render ()
-		{
-			base.NameSpaces.Add ("scms");
-		}
-		#endregion
+public class Render : SorentoLib.Addins.IRenderBaseClass, SorentoLib.Addins.IRender
+{
+#region Constructor
+public Render ()
+{
+base.NameSpaces.Add ("scms");
+}
+#endregion
 
-		#region Public Methods
-		override public object Process (SorentoLib.Session Session, string Fullname, string Method, object Variable, SorentoLib.Render.Resolver.Parameters Parameters)
-		{
-			switch (Fullname)
-			{
-//				#region sCMS.Page
-//				case "scms.page":
+#region Public Methods
+override public object Process (SorentoLib.Session Session, string Fullname, string Method, object Variable, SorentoLib.Render.Resolver.Parameters Parameters)
+{
+switch (Fullname)
+{
+// #region sCMS.Page
+// case "scms.page":
 //
-//					switch (Method)
-//					{
-//						case "":
-//							return ((sCMS.Page)Variable);
-//						
-//						case "id":
-//							return ((sCMS.Page)Variable).Id;
+// switch (Method)
+// {
+// case "":
+// return ((sCMS.Page)Variable);
 //
-//						case "createtimestamp":
-//							return ((sCMS.Page)Variable).CreateTimestamp;
+// case "id":
+// return ((sCMS.Page)Variable).Id;
 //
-//						case "updatetimestamp":
-//							return ((sCMS.Page)Variable).UpdateTimestamp;
+// case "createtimestamp":
+// return ((sCMS.Page)Variable).CreateTimestamp;
 //
-//						case "name":
-//							return ((sCMS.Page)Variable).Name;
+// case "updatetimestamp":
+// return ((sCMS.Page)Variable).UpdateTimestamp;
 //
-//						case "path":
-//							return ((sCMS.Page)Variable).Path;
+// case "name":
+// return ((sCMS.Page)Variable).Name;
 //
-//						case "template":
-//							return ((sCMS.Page)Variable).Template;
+// case "path":
+// return ((sCMS.Page)Variable).Path;
 //
-//						case "content":
-//							switch (Parameters.Type (0).Name.ToLower())
-//							{
-//								case "guid":
-//									return ((sCMS.Page)Variable).GetContent (Parameters.Get<Guid>(0));
-//									break;
+// case "template":
+// return ((sCMS.Page)Variable).Template;
 //
-//								case "string":
-//									return ((sCMS.Page)Variable).GetContent (Parameters.Get<string>(0));
-//									break;
-//							}
-//							break;
+// case "content":
+// switch (Parameters.Type (0).Name.ToLower())
+// {
+// case "guid":
+// return ((sCMS.Page)Variable).GetContent (Parameters.Get<Guid>(0));
+// break;
 //
-//						case "load":
-//							return sCMS.Page.Load (Parameters.Get<Guid>(0));
+// case "string":
+// return ((sCMS.Page)Variable).GetContent (Parameters.Get<string>(0));
+// break;
+// }
+// break;
 //
-//						case "list":
-//							return sCMS.Page.List ();
-//					}
-//					break;
-//				#endregion
+// case "load":
+// return sCMS.Page.Load (Parameters.Get<Guid>(0));
+//
+// case "list":
+// return sCMS.Page.List ();
+// }
+// break;
+// #endregion
 
-//				#region sCMS.Template
-//				case "scms.template":
+// #region sCMS.Template
+// case "scms.template":
 //
-//					switch (Method)
-//					{
-//						case "":
-//							return ((sCMS.Template)Variable);
+// switch (Method)
+// {
+// case "":
+// return ((sCMS.Template)Variable);
 //
-//						case "id":
-//							return ((sCMS.Template)Variable).Id;
+// case "id":
+// return ((sCMS.Template)Variable).Id;
 //
-//						case "createtimestamp":
-//							return ((sCMS.Template)Variable).CreateTimestamp;
+// case "createtimestamp":
+// return ((sCMS.Template)Variable).CreateTimestamp;
 //
-//						case "updatetimestamp":
-//							return ((sCMS.Template)Variable).UpdateTimestamp;
+// case "updatetimestamp":
+// return ((sCMS.Template)Variable).UpdateTimestamp;
 //
-//						case "name":
-//							return ((sCMS.Template)Variable).Title;
+// case "name":
+// return ((sCMS.Template)Variable).Title;
 //
-//						case "fields":
-//							return SorentoLib.Render.Variables.ConvertToListObject<sCMS.Field> (((sCMS.Template)Variable).Fields);
-//						
-//						case "list":
-////							if (Session.AccessLevel < SorentoLib.Enums.Accesslevel.Editor) throw new Exception (string.Format (sCMS.Strings.Exception.ResolverSessionPriviliges, "template.list"));
+// case "fields":
+// return SorentoLib.Render.Variables.ConvertToListObject<sCMS.Field> (((sCMS.Template)Variable).Fields);
 //
-//							return SorentoLib.Render.Variables.ConvertToListObject<sCMS.Template> (sCMS.Template.List ());
-//					}
-//					break;
-//				#endregion
-
-				#region sCMS.Field
-				case "scms.field":
-					switch (Method)
-					{
-						case "":
-							return ((sCMS.Field)Variable);
-
-						case "id":
-							return ((sCMS.Field)Variable).Id;
-
-						case "name":
-							return ((sCMS.Field)Variable).Name;
-
-						case "type":
-							return ((sCMS.Field)Variable).Type;
-					}
-					break;
-				#endregion
-
-				#region sCMS.Content
-				case "scms.contentdata":
-
-					switch (Method)
-					{
-						case "":
-							return ((sCMS.Content)Variable);
-
-						case "data":
-							return ((sCMS.Content)Variable).Data;
-					}
-					break;
-				#endregion
-
-//				#region sCMS.CollectionSchema
-//				case "scms.collectionschema":
+// case "list":
+//// if (Session.AccessLevel < SorentoLib.Enums.Accesslevel.Editor) throw new Exception (string.Format (sCMS.Strings.Exception.ResolverSessionPriviliges, "template.list"));
 //
-//					switch (Method)
-//					{
-//						case "":
-//							return ((sCMS.CollectionSchema)Variable);
+// return SorentoLib.Render.Variables.ConvertToListObject<sCMS.Template> (sCMS.Template.List ());
+// }
+// break;
+// #endregion
+
+#region sCMS.Field
+case "scms.field":
+switch (Method)
+{
+case "":
+return ((sCMS.Field)Variable);
+
+case "id":
+return ((sCMS.Field)Variable).Id;
+
+case "name":
+return ((sCMS.Field)Variable).Name;
+
+case "type":
+return ((sCMS.Field)Variable).Type;
+}
+break;
+#endregion
+
+#region sCMS.Content
+case "scms.contentdata":
+
+switch (Method)
+{
+case "":
+return ((sCMS.Content)Variable);
+
+case "data":
+return ((sCMS.Content)Variable).Data;
+}
+break;
+#endregion
+
+// #region sCMS.CollectionSchema
+// case "scms.collectionschema":
 //
-//						case "name":
-//							return ((sCMS.CollectionSchema)Variable).Name;
+// switch (Method)
+// {
+// case "":
+// return ((sCMS.CollectionSchema)Variable);
 //
-//						case "collections":
-//							return ((sCMS.CollectionSchema)Variable).Collections;
+// case "name":
+// return ((sCMS.CollectionSchema)Variable).Name;
 //
-//						case "load":
-//							switch (Parameters.Type (0).Name.ToLower())
-//							{
-//								case "guid":
-//									return sCMS.CollectionSchema.Load (Parameters.Get<Guid>(0));
+// case "collections":
+// return ((sCMS.CollectionSchema)Variable).Collections;
 //
-//								case "string":
-//									return sCMS.CollectionSchema.Load (new Guid (Parameters.Get<string>(0)));
-//							}
-//							break;
-//					}
-//					break;
-//				#endregion
-
-				#region sCMS.Collection
-				case "scms.collection":
-
-					switch (Method)
-					{
-						case "":
-							return ((sCMS.Collection)Variable);
-
-						case "id":
-							return ((sCMS.Collection)Variable).Id;
-
-						case "name":
-//							return ((sCMS.Collection)Variable).Name;
-
-//						#region Name
-//						case "content":
-//							return ((sCMS.Collection)Variable).Contents;
-//						#endregion
-
-						case "content":
-//							switch (Parameters.Type (0).Name.ToLower())
-//							{
-//								case "guid":
-//									return ((sCMS.Collection)Variable).GetContent (Parameters.Get<Guid>(0));
+// case "load":
+// switch (Parameters.Type (0).Name.ToLower())
+// {
+// case "guid":
+// return sCMS.CollectionSchema.Load (Parameters.Get<Guid>(0));
 //
-//								case "string":
-//									return ((sCMS.Collection)Variable).GetContent (Parameters.Get<string>(0));
-//							}
-							break;
+// case "string":
+// return sCMS.CollectionSchema.Load (new Guid (Parameters.Get<string>(0)));
+// }
+// break;
+// }
+// break;
+// #endregion
 
-						case "load":
-							switch (Parameters.Type (0).Name.ToLower())
-							{
-								case "guid":
-									return sCMS.Collection.Load (Parameters.Get<Guid>(0));
+#region sCMS.Collection
+case "scms.collection":
 
-								case "string":
-									return sCMS.Collection.Load (new Guid (Parameters.Get<string>(0)));
-							}
-							break;
-					}
-					break;
-				#endregion
-			}
-			
-			
-			
-			throw new SorentoLib.Exceptions.RenderExceptionMemberNotFound ();
-		}
-		#endregion
-	}
+switch (Method)
+{
+case "":
+return ((sCMS.Collection)Variable);
+
+case "id":
+return ((sCMS.Collection)Variable).Id;
+
+case "name":
+// return ((sCMS.Collection)Variable).Name;
+
+// #region Name
+// case "content":
+// return ((sCMS.Collection)Variable).Contents;
+// #endregion
+
+case "content":
+// switch (Parameters.Type (0).Name.ToLower())
+// {
+// case "guid":
+// return ((sCMS.Collection)Variable).GetContent (Parameters.Get<Guid>(0));
+//
+// case "string":
+// return ((sCMS.Collection)Variable).GetContent (Parameters.Get<string>(0));
+// }
+break;
+
+case "load":
+switch (Parameters.Type (0).Name.ToLower())
+{
+case "guid":
+return sCMS.Collection.Load (Parameters.Get<Guid>(0));
+
+case "string":
+return sCMS.Collection.Load (new Guid (Parameters.Get<string>(0)));
+}
+break;
+}
+break;
+#endregion
+}
+
+
+
+throw new SorentoLib.Exceptions.RenderExceptionMemberNotFound ();
+}
+#endregion
+}
 }
--- Source/sCMS/sCMS/Runtime.cs
+++ Source/sCMS/sCMS/Runtime.cs
@@ -42,40 +42,51 @@ namespace sCMS
 		
 		public static void Initialize ()
 		{				
-			sConsole.Menu.AddCategory ("scms", "sCMS", 10);
-			sConsole.Menu.AddItem ("scms", "content", "Content", "/scms/content/", 1);
-			sConsole.Menu.AddItem ("scms", "styling", "Construction", "/scms/construction/", 2);
-			sConsole.Menu.AddItem ("scms", "settings", "Settings", "/scms/settings/", 3);
-						
-			// Create symlinks if they dont exist.
-			if (!Directory.Exists (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/content/scms"))
+			try
 			{
-				UnixFileInfo dirinfo = new UnixFileInfo (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sCMS/data/content/scms");			
-				dirinfo.CreateSymbolicLink (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/content/scms");
-			}
+				Include.Add (sConsole.Enums.IncludeType.Javascript, "/js/scms.js", "SCMS", 101);
+				
+				sConsole.Menu.AddCategory ("scms", "sCMS", 10);
+				sConsole.Menu.AddItem ("scms", "content", "Content", "/scms/content/", 1);
+				sConsole.Menu.AddItem ("scms", "styling", "Construction", "/scms/construction/", 2);
+				sConsole.Menu.AddItem ("scms", "settings", "Settings", "/scms/settings/", 3);
+		
+				// Create symlinks if they dont exist.
+				if (!Directory.Exists (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/content/scms"))
+				{
+					UnixFileInfo dirinfo = new UnixFileInfo (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sCMS/data/content/scms");			
+					dirinfo.CreateSymbolicLink (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/content/scms");
+				}
 			
-			if (!Directory.Exists (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/html/xml/scms"))
-			{
-				UnixFileInfo dirinfo = new UnixFileInfo (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sCMS/data/html/xml/scms");
-				dirinfo.CreateSymbolicLink (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/html/xml/scms");
-			}
+				if (!Directory.Exists (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/html/xml/scms"))
+				{
+					UnixFileInfo dirinfo = new UnixFileInfo (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sCMS/data/html/xml/scms");
+					dirinfo.CreateSymbolicLink (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/html/xml/scms");
+				}
 			
-			if (!File.Exists (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/html/js/scms.js"))
-			{
-				UnixFileInfo dirinfo = new UnixFileInfo (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sCMS/data/html/js/scms.js");
-				dirinfo.CreateSymbolicLink (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/html/js/scms.js");
-			}
+				if (!File.Exists (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/html/js/scms.js"))
+				{
+					UnixFileInfo dirinfo = new UnixFileInfo (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sCMS/data/html/js/scms.js");
+					dirinfo.CreateSymbolicLink (SorentoLib.Services.Config.Get<string> (SorentoLib.Enums.ConfigKey.path_addins) + "sConsole/data/html/js/scms.js");
+				}
 										
-			// Set defaults
-			SetDefaults ();
+				// Set defaults
+				SetDefaults ();
 			
-			// Set default usergroups
+				// Set default usergroups
 //			UsergroupGuest = Usergroup.AddBuildInUsergroup (new Guid ("10de93c3-4d70-445a-8457-97beefd6809d"), "sCMS Guest");			
 //			UsergroupUser = Usergroup.AddBuildInUsergroup (new Guid ("50176827-ae08-42cf-aefb-2db768a9b9e1"), "sCMS User");
 //			UsergroupModerator = Usergroup.AddBuildInUsergroup (new Guid ("a9754690-646c-4971-96cd-ba16716dcfe4"), "sCMS Moderator");
-			UsergroupEditor =	Usergroup.AddBuildInUsergroup (new Guid ("a0a2cf36-fa74-43a7-8f5f-34aa837cccef"), "sCMS Editor");
-			UsergroupAuthor =	Usergroup.AddBuildInUsergroup (new Guid ("20e8185e-18a2-4178-a99a-157c2a360426"), "sCMS Author");
-			UsergroupAdministrator = Usergroup.AddBuildInUsergroup (new Guid ("3b9ca3c0-67e9-4750-aacf-51b8faa4527f"), "sCMS Administrator");
+				UsergroupEditor =	Usergroup.AddBuildInUsergroup (new Guid ("a0a2cf36-fa74-43a7-8f5f-34aa837cccef"), "sCMS Editor");
+				UsergroupAuthor =	Usergroup.AddBuildInUsergroup (new Guid ("20e8185e-18a2-4178-a99a-157c2a360426"), "sCMS Author");
+				UsergroupAdministrator = Usergroup.AddBuildInUsergroup (new Guid ("3b9ca3c0-67e9-4750-aacf-51b8faa4527f"), "sCMS Administrator");
+			}
+			catch (Exception exception)
+			{
+				// LOG: LogDebug.ExceptionUnknown
+				SorentoLib.Services.Logging.LogDebug (string.Format (SorentoLib.Strings.LogDebug.ExceptionUnknown, "SCMS.INITIALIZE", exception.Message));
+			}
+				
 		}
 		
 		private static void SetDefaults ()
