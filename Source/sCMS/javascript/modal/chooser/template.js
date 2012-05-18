4162589a7470dd6e5670fd80cb61502d9e6da92a
Rasmus Pedersen <rasmus@akvaservice.dk>
2012-05-18 16:13:11 +0200

    -

--- Source/sCMS/javascript/modal/chooser/template.js
+++ Source/sCMS/javascript/modal/chooser/template.js
@@ -1,5 +1,24 @@
 template : function (attributes)
 {
+	// SET			
+	var set =		function ()
+					{
+						chooser.getUIElement ("templates").setItems (sCMS.template.list ());										
+					};
+
+	// ONINIT					
+	var onInit =	function ()
+					{				
+						chooser.getUIElement ("templates").setAttribute ("onChange", onChange);
+				
+						// SET
+						set ();
+				
+						// SHOW
+						chooser.show ();							
+					};
+
+	// ONBUTTON1
 	var onButton1 =	function ()
 					{
 						chooser.dispose ();
@@ -9,7 +28,8 @@ template : function (attributes)
 							setTimeout( function ()	{ attributes.onDone (chooser.getUIElement ("templates").getItem ()); }, 1);
 						}
 					};
-					
+				
+	// ONBUTTON2	
 	var onButton2 =	function ()
 					{
 						chooser.dispose ();
@@ -20,6 +40,7 @@ template : function (attributes)
 						}						
 					};
 					
+	// ONCHANGE	
 	var onChange = 	function ()
 					{
 						if (chooser.getUIElement ("templates").getItem ())
@@ -31,31 +52,26 @@ template : function (attributes)
 							chooser.getUIElement ("button1").setAttribute ("disabled", true);
 						}
 					};					
-
+					
 	var suixml = "";
 	suixml += '<sui>';
-	suixml += '	<layoutbox type="horizontal">';
-	suixml += '		<panel size="*">';
-	suixml += '			<layoutbox type="vertical">';
-	suixml += '				<panel size="*">';
+//	suixml += '	<layoutbox type="horizontal">';
+//	suixml += '		<panel size="*">';
+//	suixml += '			<layoutbox type="vertical">';
+//	suixml += '				<panel size="*">';
 	suixml += '					<listview tag="templates" width="100%" height="100%" focus="true" treeview="true" treeviewLinkColumns="id:parentid" treeviewRootValue="00000000-0000-0000-0000-000000000000">';
 	suixml += '						<column tag="id" />';
 	suixml += '						<column tag="title" label="Title" width="200px" visible="true" />';	
 	suixml += '						<column tag="parentid" />'
 	suixml += '					</listview>';
-	suixml += '				</panel>';
-	suixml += '			</layoutbox>';
-	suixml += '		</panel>';
-	suixml += '	</layoutbox>';
+//	suixml += '				</panel>';
+//	suixml += '			</layoutbox>';
+//	suixml += '		</panel>';
+//	suixml += '	</layoutbox>';
 	suixml += '</sui>';
 
-	var chooser = new sConsole.modal.chooser.base ({suiXML: suixml, title: "Choose template", buttonLabel: "Ok|Cancel", onClickButton1: onButton1, onClickButton2: onButton2});
-	
-	chooser.getUIElement ("templates").setItems (sCMS.template.list ());
-	chooser.getUIElement ("templates").setAttribute ("onChange", onChange);
-				
-	chooser.show ();			
-}	
+	var chooser = new sConsole.modal.chooser.base ({suiXML: suixml, title: "Choose template", buttonLabel1: "Select", buttonLabel2: "Close", onClickButton1: onButton1, onClickButton2: onButton2, onInit: onInit});
+}
 
 
 
